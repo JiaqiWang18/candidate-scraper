038849cf8534eda26905f9bc93016889695a1482
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-09-11 22:10:31 -0400

    untrack out/ and .idea/

--- scraper.py
+++ scraper.py
@@ -11,7 +11,8 @@ from pathlib import Path
 parser = argparse.ArgumentParser(description="Scrapes candidate issue info")
 parser.add_argument("name", metavar="N", type=str, help="Name of candidate")
 parser.add_argument("url", metavar="U", type=str, help="URL of candidate")
-parser.add_argument("-o", "--outfile", dest='outfile', type=str, required=False, help="location of the output file (default is out)")
+parser.add_argument("-o", "--outfile", dest='outfile', type=str, required=False,
+                    help="location of the output file (default is out)")
 
 parser.add_argument(
     "issue_pattern",
@@ -53,6 +54,7 @@ parser.add_argument(
 args = parser.parse_args()
 pp = pprint.PrettyPrinter(indent=4)
 
+
 # Generates beautiful soup object
 def get_page_soup(url):
     headers = {
@@ -61,7 +63,7 @@ def get_page_soup(url):
     if not args.invalid:
         page = requests.get(url, headers=headers).content
     else:
-        page = requests.get(url, headers=headers,verify=False).content
+        page = requests.get(url, headers=headers, verify=False).content
 
     return BeautifulSoup(page, "html.parser")
 
@@ -69,72 +71,97 @@ def get_page_soup(url):
 # Gets the innermost children of the passed elements given the names where
 # names is a list of pairs of strings (element_name, element_class)
 def get_children(names, elements):
+    print(f"names: {names}")
+    # iterate through pattern pairs
     for pair in names:
         children = []
+        # iterate through parent elements
         for i in range(0, len(elements)):
             if elements[i] is not None:
+                # if patter pair has class, use the class
                 if pair[1]:
+                    # set the parent element to the children that matches the pattern pair
                     elements[i] = elements[i].findChildren(
                         pair[0], {"class", pair[1]}, recursive=False
                     )
+                # do not use class if no class
                 else:
+                    # set the parent element to the children that matches the pattern pair
                     elements[i] = elements[i].findChildren(pair[0], recursive=False)
+                # if found valid children, add the array of children to existing children
                 if elements[i] is not None:
                     children.extend(elements[i])
+                # if did not find valid children, set the element to None
                 else:
                     elements[i] = None
+        # after iterating through parent elements, set parent elements to the valid child elements, then recursion
         elements = children
-
     return elements
 
 
 # Returns either the text elements or links in a given soup object by pattern
 def get_text_by_pattern(soup, pattern, get_link=False):
+    print(pattern)
     element_names_unformatted = pattern.split(">")
     element_names = []
+    # loop that parse pattern and store a list of tag, class pair
     for el_name in element_names_unformatted:
         if "#" in el_name:
             el_parts = el_name.split("#")
             element_names.append((el_parts[0], el_parts[1]))
         else:
             element_names.append((el_name, None))
-
+    print(element_names)
     elements = None
+    # now find parent elements
     if element_names[0][1]:
+        # if has class name, find with class
         elements = soup.find_all(element_names[0], {"class": element_names[0][1]})
     else:
+        # find without class
         elements = soup.find_all(element_names[0])
-
+    # elements contain all parent elements found, now we find children of that element that matches the pattern
     elements = list(get_children(element_names[1:], elements))
+    # get rid of None or empty strings
     elements = list(filter(lambda x: x is not None and len(x) != 0, elements))
+    # elements now contain all the children that matches the pattern
 
     wanted = []
     for el in elements:
+        # set final output as text if element is not anchor tag
         if not get_link:
             wanted.append(el.text.strip())
+        # set final output as link if element is anchor tag
         else:
             wanted.append(el["href"])
     return list(dict.fromkeys(wanted))
 
 
+# returns an array of string issues of this candidate
 def get_issues(soup):
     issues = get_text_by_pattern(soup, args.issue_pattern)
     return issues
 
 
-def get_descriptions(soup,url):
+def get_descriptions(soup, url):
     descriptions = []
     if args.follow_link:
+        # get the links to each issue descriptions
         links = get_text_by_pattern(soup, args.follow_link, get_link=True)
         print(links)
         for link in links:
+            # construct full url if link is relative url
             if link[0] == "/":
-                link= url.rsplit('/', 1)[0] + link
+                link = url.rsplit('/', 1)[0] + link
+            # get soup obj of new page
             soup = get_page_soup(link)
+            # get the issue description by using the description pattern arg
             description = get_text_by_pattern(soup, args.description_pattern)
+            # convert paragraphs of description into a string
             description = "\n".join(description)
             descriptions.append(description.strip())
     else:
+        # if all on same page, just get the description
         descriptions = get_text_by_pattern(soup, args.description_pattern)
     return descriptions
 
@@ -142,24 +169,25 @@ def get_descriptions(soup,url):
 def main():
     soup = get_page_soup(args.url)
     issues = list(filter(None, get_issues(soup)))
-    descriptions = list(filter(None, get_descriptions(soup,args.url)))
+    descriptions = list(filter(None, get_descriptions(soup, args.url)))
     print(issues)
-    for i,desc in enumerate(descriptions):
+    for i, desc in enumerate(descriptions):
         print(f"======= {i} =======")
         print(desc)
+    # check if there is an issue description correspond to each issue
     if len(issues) != len(descriptions) and not args.force:
         print(f"Error: Issue length({len(issues)}) does not match description length({len(descriptions)})")
         return
-
-    if(args.outfile):
+    if (args.outfile):
         path = args.outfile
     else:
         path = 'out'
-		
+
     Path(path).mkdir(parents=True, exist_ok=True)
 
     filename = path + "/" + args.name.replace(' ', '_') + '.csv'
     length = len(issues)
+    # add empty string to whichever is not parsed successfully
     if len(issues) < len(descriptions):
         length = len(descriptions)
         while len(issues) < length:
@@ -167,14 +195,13 @@ def main():
     else:
         while len(descriptions) < length:
             descriptions.append("")
-    
 
-    with open(filename,'w') as csvfile:
+    with open(filename, 'w') as csvfile:
         writer = csv.writer(csvfile)
         for i in range(length):
-            writer.writerow([args.name, ftfy.fix_text(issues[i]), 
-                ftfy.fix_text(descriptions[i]), args.url])
-        writer.writerow(["","","",""])
+            writer.writerow([args.name, ftfy.fix_text(issues[i]),
+                             ftfy.fix_text(descriptions[i]), args.url])
+        writer.writerow(["", "", "", ""])
 
 
 main()
