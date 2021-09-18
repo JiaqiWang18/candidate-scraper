e8cca6a0aa8e921e242c4d416ab0a94a0fe10742
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-09-18 09:54:38 -0400

    finish

--- scraper.py
+++ scraper.py
@@ -173,7 +173,7 @@ def main():
     print(issues)
     for i, desc in enumerate(descriptions):
         print(f"======= {i} =======")
-        print(f"{desc[:10]}....{desc[-10:-1]}")
+        print(f"{desc[:20]}....{desc[-20:-1]}")
     # check if there is an issue description correspond to each issue
     if len(issues) != len(descriptions) and not args.force:
         print(f"Error: Issue length({len(issues)}) does not match description length({len(descriptions)})")
